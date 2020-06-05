aa3d160b8a00d4d46312fdc14d7d3549c75721d1
Trevor Love <trevorlove@Trevors-MacBook-Pro.local>
2020-06-05 06:08:39 -0400

    add initial dependencies, partials, and homepage

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const request = require("request-promise");
+
+app.set("view engine", "ejs");
+
+app.listen(3000, () => {
+  console.log("App is now listening on port 3000");
+});
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
