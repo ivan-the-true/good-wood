61bd7bafeb9116acb7ecfcacfa9ac1289604efcd
Trevor Love <trevorlove@Trevors-MacBook-Pro.local>
2020-06-05 10:32:16 -0400

    added page for viewing campgrounds and page for superficially adding new campgrounds

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const request = require("request-promise");
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.listen(3000, () => {
@@ -11,3 +13,30 @@ app.listen(3000, () => {
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.get("/campgrounds", (req, res) => {
+  res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", (req, res) => {
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = { name: name, image: image}
+  campgrounds.push(newCampground);
+  res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("new");
+});
+
+var campgrounds = [
+  {
+    name: "Cloudland Canyon",
+    image: "https://images.unsplash.com/photo-1522889126620-04b9f7196cd0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+  },
+  {
+    name: "Providence Canyon",
+    image: "https://images.unsplash.com/photo-1573681620389-69cab4c82ae4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+  },
+]
