4d8bef4a1e9cb3a43032972ff910be96142da58a
Trevor Love <trevorlove@Trevors-MacBook-Pro.local>
2020-06-09 08:56:08 -0400

    added subdirectories for campground and comment views. Added 'new' comment view. Added new comment route. Added create comment route.

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const express = require("express"),
       mongoose = require("mongoose"),
       Campground = require("./models/campground"),
       seedDB = require("./seeds");
-      //Comment = require("./models/comment")
+      Comment = require("./models/comment")
 
-seedDB();
+// seedDB();
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useNewUrlParser', true);
 mongoose.connect("mongodb://localhost/good_wood");
@@ -31,7 +31,7 @@ app.get("/campgrounds", (req, res) => {
       console.log("Error:");
       console.log(err);
     } else {
-      res.render("index", {campgrounds: allCampgrounds})
+      res.render("campgrounds/index", {campgrounds: allCampgrounds})
     }
   });
 });
@@ -57,7 +57,7 @@ app.post("/campgrounds", (req, res) => {
 });
 
 app.get("/campgrounds/new", (req, res) => {
-  res.render("new");
+  res.render("campgrounds/new");
 });
 
 app.get("/campgrounds/:id", (req, res) => {
@@ -65,7 +65,45 @@ app.get("/campgrounds/:id", (req, res) => {
     if (err) {
       console.log(err);
     } else {
-      res.render("show", {campground: foundCampground});
+      res.render("campgrounds/show", {campground: foundCampground});
     }
   });
 });
+
+
+//==========================
+//Comments Routes
+//==========================
+
+app.get("/campgrounds/:id/comments/new", (req, res) => {
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if (err) {
+      console.log('Error! - ');
+      console.log(err);
+    } else {
+      res.render("comments/new", {campground: foundCampground});
+    }
+  })
+});
+
+app.post("/campgrounds/:id/comments", (req, res) => {
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if (err) {
+      console.log('Error! - ');
+      console.log(err);
+      res.redirect("/campgrounds")
+    } else {
+      console.log(req.body.comment);
+      Comment.create(req.body.comment, (err, comment) => {
+        if (err) {
+          console.log('Error! - ');
+          console.log(err);
+        } else {
+          foundCampground.comments.push(comment);
+          foundCampground.save();
+          res.redirect("/campgrounds/" + foundCampground._id);
+        }
+      })
+    }
+  })
+})
